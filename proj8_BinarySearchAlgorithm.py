abe2f4b85023ba22a55f142ea5e1bc642cdc3cd0
abhishekmenon1 <83978081+abhishekmenon1@users.noreply.github.com>
2021-10-12 17:37:10 -0400

    Add files via upload
    You can create a list of random numbers between 0 to 100, with every succeeding number having a difference of 2 between them. When the user inputs a random number, the program will check if that number is included in the list. It will do so by creating two halves of the list. If the program finds the number in the first half of the list, it will eliminate the other half and vice versa. The search will continue until the program finds the number input of the user or until the subarray size becomes 0 (this means that the number is not in the list).

---
+++ proj8_BinarySearchAlgorithm.py
@@ -0,0 +1,32 @@
+position = 0
+
+def binary(set, number):
+    low_limit = 0
+    upper_limit = len(set) - 1
+
+    while low_limit <= upper_limit:
+        n = (low_limit + upper_limit)//2
+        if number == set[n]:
+            globals()['position'] = n
+            return True
+        else:
+            if number < set[n]:
+                upper_limit = n
+            else:
+            #elif number > set[n]:
+                low_limit = n
+
+    return False
+
+set = []
+for i in range(0, 100, 2):
+    set.append(i)
+
+list(map(int, set))
+print(set)
+
+number = int(input("Enter A Number: "))
+
+if binary(set, number):
+    #if true
+    print("Number is at position", position + 1)
