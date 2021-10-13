66fd6f41af3c4d793fa4b5fe6ce7361a9d7a0a37
abhishekmenon1 <83978081+abhishekmenon1@users.noreply.github.com>
2021-10-12 21:16:23 -0400

    Add files via upload
    Used "plyer" as the notification library.

---
+++ proj9_desktop notification.py
@@ -0,0 +1,9 @@
+from plyer import notification
+
+notification.notify(title = "Hello!",
+                    message = "This is a test message. I am under the water. This is lays chips ahoy cookies",
+                    app_name = "Things",
+                    app_icon = 'C:/Users/User/PycharmProjects/Lays-Logo.ico',
+                    timeout = 7,
+                    toast = False,
+)
